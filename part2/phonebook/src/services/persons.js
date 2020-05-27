041aea1c105a12f7809fdf1d920041e1c55c0dce
Ryan Filax-Wylie <ryanfilaxwylie@gmail.com>
2020-05-27 06:20:48 -0600

    Changed to use heroku server

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseUrl = "http://localhost:3001/persons";
+const baseUrl = "https://agile-springs-10319.herokuapp.com/api/persons";
 
 const addPerson = (newPerson) => {
   const request = axios.post(baseUrl, newPerson);
